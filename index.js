62f5e83dbb2085a6bd18b431c3be141d94fca049
Zach Monroe <z@localhost.localdomain>
2024-03-20 11:58:21 -0400

    User can add books to database
    user can now add their books to the databse. Data is used to find isbn number and add to isbn database.
    
    TODOs are Updated
    Edge Case to be handled: User inputs wrong data.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = new pg.Client({
   host: "localhost",
   database: "books",
   user: "postgres",
-  password: "password here",
+  password: "your password here",
   port: 5432,
 });
 
@@ -25,27 +25,66 @@ app.get("/", async (req, res) => {
   );
   console.log(result.rows);
   const books = result.rows;
-  const isbn = await db.query("SELECT * FROM isbn");
   res.render("index.ejs", { books: books });
 });
+
 app.get("/new", async (req, res) => {
   res.render("new.ejs");
 });
+
 app.post("/add", async (req, res) => {
   console.log(req.body);
-  // db query for adding a book to read
-  //set up html form
-  //  naming:
-  //  const title = req.body.title
+  //For adding the users input into the database
+  const title = req.body.title;
+  const notes = req.body.notes;
+  const author_fname = req.body.author_fname;
+  const author_lname = req.body.author_lname;
+  const rating = req.body.rating;
+  let id;
+  try {
+    const readResult = await db.query(
+      "INSERT INTO read (author_lname, title, notes, rating, author_fname) VALUES ($1, $2, $3, $4, $5) RETURNING (id)",
+      [author_lname, title, notes, rating, author_fname],
+    );
+
+    id = readResult.rows[0].id;
+  } catch (err) {
+    console.log(err.body);
+  }
+
+  //API request for getting the ISBN
+  const urlTitle = title.toLowerCase().replace(/ /g, "+");
+  const urlAuthor = author_lname.toLowerCase();
+  const isbnGet = await axios.get(
+    "https://openlibrary.org/search.json?title=" +
+      urlTitle +
+      "&author=" +
+      urlAuthor,
+  );
+  const isbnResult = isbnGet.data;
+  const isbn = JSON.parse(isbnResult.docs[0].isbn[0]);
+  try {
+    await db.query("INSERT INTO isbn (book_id, book_isbn) VALUES ($1, $2)", [
+      id,
+      isbn,
+    ]);
+  } catch (err) {
+    console.log(err.body);
+  }
+  res.redirect("/");
+
+  // FIX: Edge case - user accidentally inputs data that yields no result from search API.
+  // should stop the user from posting to the database and leave their input in the form.
+  // Should inform them the book they want to log does not exist.
+  // Maybe reconfigure so it searches for the isbn BEFORE adding to "read" database - preventing unneccessary insertion and deletion.
 });
 
 app.listen(port, () => {
   console.log(`Server is live at port ${port}`);
 });
 
-// TODO: Add DB Query for adding a book to the read database.returning author name, title and book id.
-// TODO: Format the "/add" url endpoint to also retrieve the isbn number from the title and author information using axios
-// TODO: Add the isbn number to the isbn table.
-// TODO: add endpoints for differing sorting methods and modify the query accordingly. (Maybe use a variable to keep one endpoint and have it be set by the req.body?)
+// TODO: add endpoints for differing sorting methods and modify the query accordingly.
+//    (Maybe use a variable to keep one endpoint and have it be set by the req.body?)
+//
 // TODO: set up deletion handling
 // TODO: set up a put request for editing a post. (Might need to make a hidden html element that stores the id for editing and deleting.)
